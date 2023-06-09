0db540792b12ec04e72cdca3cde085433bad74ab
khaddan <khaddanzakaria7@gmail.com>
2023-06-09 15:09:44 +0100

    First Commit

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   public getProducts(page:number=1,size:number=4):Observable<Array<Product>>{
-    return this.http.get<Array<Product>>('http://localhost:8089/products? page=${page}&_limit=${size}');
+    return this.http.get<Array<Product>>('http://localhost:8089/products? ');//page=${page}&_limit=${size}
   }
   public checkProduct(product:Product):Observable<Array<Product>>{
     return this.http.patch<Array<Product>>('http://localhost:8089/products/' + product.id,
